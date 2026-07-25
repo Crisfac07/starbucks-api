7e4aab777ce04ead394591051adbfa553d94506a
Cris Franco <francochavezcristian@gmail.com>
2026-07-24 22:54:38 -0600

    Adding nuguet packages

--- Starbucks.Domain/Category.cs
+++ Starbucks.Domain/Category.cs
@@ -6,5 +6,8 @@ namespace Starbucks.Domain
 {
     public class Category
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
     }
 }
