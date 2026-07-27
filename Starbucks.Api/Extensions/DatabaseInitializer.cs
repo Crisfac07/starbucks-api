090e106fd440f8ddfc9b5180f31b44f1bc064627
Cris Franco <francochavezcristian@gmail.com>
2026-07-26 22:42:53 -0600

    Add automatic migration execution

---
+++ Starbucks.Api/Extensions/DatabaseInitializer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Starbucks.Persistence;
+
+namespace Starbucks.Api.Extensions
+{
+    public static class DatabaseInitializer
+    {
+        public static async Task ApplyMigration
+            (
+                this ApplicationBuilder applicationBuilder,
+                IWebHostEnvironment environment
+            ) 
+        {
+
+            using (var scope = applicationBuilder.ApplicationServices.CreateScope()) 
+            {
+                var service = scope.ServiceProvider;
+                var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = service.GetRequiredService<StarbucksDbContext>();
+                    await context.Database.MigrateAsync();
+
+                }
+                catch (Exception ex) 
+                {
+                    var logger = loggerFactory.CreateLogger<ILoggerFactory>();
+                    logger.LogError(ex, "Error during the migration");
+                }
+            }
+        }
+    }
+}
