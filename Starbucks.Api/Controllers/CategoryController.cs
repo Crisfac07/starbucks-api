93a145c8bf1f174cf1e4e8b64a10b979b269bdc1
Cris Franco <francochavezcristian@gmail.com>
2026-07-28 00:57:41 -0600

    Add CategoryController and implement GET endpoint

---
+++ Starbucks.Api/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Starbucks.Domain;
+using Starbucks.Persistence;
+
+namespace Starbucks.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly StarbucksDbContext _dbContext;
+        public CategoryController(StarbucksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<List<Category>> Get() 
+        {
+           return await _dbContext.Categories.ToListAsync();
+        }
+
+    }
+}
