f593c442f6677ddc2afc2898ed2c3ad70c9dac90
Cris Franco <francochavezcristian@gmail.com>
2026-07-28 00:44:32 -0600

    Improve database seeding for initial data population

--- Starbucks.Api/Extensions/DatabaseInitializer.cs
+++ Starbucks.Api/Extensions/DatabaseInitializer.cs
@@ -12,10 +12,10 @@ namespace Starbucks.Api.Extensions
             (
                 this WebApplication webApplication,
                 IWebHostEnvironment environment
-            ) 
+            )
         {
 
-            using (var scope = webApplication.Services.CreateScope()) 
+            using (var scope = webApplication.Services.CreateScope())
             {
                 var service = scope.ServiceProvider;
                 var loggerFactory = service.GetRequiredService<ILoggerFactory>();
@@ -26,7 +26,7 @@ namespace Starbucks.Api.Extensions
                     await DataSeed(context, environment);
 
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
                 {
                     var logger = loggerFactory.CreateLogger<ILoggerFactory>();
                     logger.LogError(ex, "Error during the migration");
@@ -35,29 +35,69 @@ namespace Starbucks.Api.Extensions
         }
 
 
-        public static async Task DataSeed(StarbucksDbContext dbContext, IWebHostEnvironment environment) {
+        public static async Task DataSeed(StarbucksDbContext dbContext, IWebHostEnvironment environment)
+        {
             if (dbContext.Coffees.Any()) return;
             if (environment is null) throw new Exception("The environment was not loaded");
-            
+
             var path = Path.Combine(environment.ContentRootPath, "Resources/coffee.json");
             var coffeeDataText = await File.ReadAllTextAsync(path);
 
             var data = JsonConvert.DeserializeObject<List<CoffeeJson>>(coffeeDataText)
                 ?? Enumerable.Empty<CoffeeJson>();
 
-            var coffees = data.Select(json => new Coffee
-            {
-                Id = json.CoffeeId,
-                Name = json.Title!,
-                Description = json.Description,
-                Price = 10,
-                CategoryId = json.Category,
-                Image = json.Image
-            }).ToArray();
+            var coffeeMaster = new List<Coffee>();
+            var ingredientMaster = new List<Ingredient>();
+            var random = new Random();
 
-            await dbContext.Coffees.AddRangeAsync(coffees);
+            foreach (var coffeeElement in data)
+            {
+                var ingredientLocal = new List<Ingredient>();
+                foreach (var ingredient in coffeeElement.Ingredients)
+                {
+                    var existingIngredient = ingredientMaster
+                        .FirstOrDefault(
+                            i => 
+                            string.Equals(
+                                i.Name, 
+                                ingredient, 
+                                StringComparison.CurrentCultureIgnoreCase
+                                ));
+                    if (existingIngredient is null)
+                    {
+                        existingIngredient = new Ingredient
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = ingredient
+                        };
+                        ingredientMaster.Add(existingIngredient);
+                    }
+                    ingredientLocal.Add(existingIngredient);
+                }
+
+                var newCoffee = new Coffee
+                { 
+                    Name = coffeeElement.Title!,
+                    Description = coffeeElement.Description,
+                    Image = coffeeElement.Image,
+                    CategoryId = coffeeElement.Category,
+                    Price = RandomPrice(random, 2, 15),
+                    Ingredients = ingredientLocal
+                };
+
+                coffeeMaster.Add(newCoffee);
+            }
+
+            await dbContext.Ingredients.AddRangeAsync(ingredientMaster);
+            await dbContext.Coffees.AddRangeAsync(coffeeMaster);
             await dbContext.SaveChangesAsync();
 
+
+        }
+
+        public static decimal RandomPrice(Random random, double min, double max)
+        {
+           return Convert.ToDecimal(Math.Round(random.NextDouble() * Math.Abs(max-min) + min, 2));
         }
     }
 }
