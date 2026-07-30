f105d085f8cbc1e038a12fa9af23fd9366e70cba
Cris Franco <francochavezcristian@gmail.com>
2026-07-30 14:22:12 -0600

    Create Core.Mappy project

---
+++ Core/Core.Mappy/Interfaces/IMapper.cs
@@ -0,0 +1,19 @@
+﻿using Core.Mappy.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Mappy.Interfaces
+{
+    public interface IMapper
+    {
+        void CreateMap<TSource, TDestination>();
+
+        void CreateMap<TSource, TDestination>(
+            Action<MapperConfiguration<TSource, TDestination>> configure);
+
+        TDestination Map<TDestination>(object source);
+
+    }
+
+}
---
+++ Core/Core.Mappy/Interfaces/IMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Mappy.Interfaces
+{
+    public interface IMappingProfile
+    {
+        void Configure(IMapper mapper);
+    }
+}
