54e96c93e33b55f730e32b464eee878fefb3a0c6
Cris Franco <francochavezcristian@gmail.com>
2026-08-06 01:04:07 -0600

    Create Result class to implement Result pattern

---
+++ Starbucks.Application/Abstractions/Result.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starbucks.Application.Abstractions
+{
+
+    public abstract class GlobalResult 
+    {
+        public bool IsSuccess { get; set; }
+        public bool IsFailure => !IsSuccess;
+
+        public List<Error> Errors { get; set; } = new();
+        protected GlobalResult()
+        {
+
+        }
+    }
+    public class Result<T> :GlobalResult
+    {
+        
+        public T? Value { get; set; }
+        
+        public static Result<T> Success(T value) => new Result<T>
+        {
+            IsSuccess = true,
+            Value = value
+        };
+
+        public static Result<T> Failure(params Error[] errors) => new Result<T>
+        {
+            IsSuccess = false,
+            Errors = errors.ToList()
+        };
+    }
+    public class Result : GlobalResult
+    {
+        
+        public static Result Success() => new Result
+        {
+            IsSuccess = true
+        };
+        public static Result Failure(params Error[] errors) => new Result
+        {
+            IsSuccess = false,
+            Errors = errors.ToList()
+        };
+
+    }
+}
